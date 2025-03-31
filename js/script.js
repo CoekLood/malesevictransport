599e16b63d4cb155262f12091dea38ec9f59cf78
CoekLood <dakiyt9@gmail.com>
2025-03-31 21:13:02 +0200

    Added window resize handle for burger menu to automatically close when resizing above certain width, finishhed responsive

--- js/script.js
+++ js/script.js
@@ -43,3 +43,17 @@ function toggleMenu() {
   var menu = document.getElementById('burgerMenu');
   menu.classList.toggle('open');
 }
+
+function handleResize() {
+  var menu = document.getElementById('burgerMenu');
+  var burgerIcon = document.querySelector('.burgerIcon');
+  
+  // If the window width is larger than 768px, close the burger menu and remove the animation
+  if (window.innerWidth > 768) {
+    menu.classList.remove('open'); // Hide the burger menu
+    burgerIcon.classList.remove('change'); // Remove the animation from the burger icon
+  }
+}
+
+// Listen for window resize and run handleResize function
+window.addEventListener('resize', handleResize);
